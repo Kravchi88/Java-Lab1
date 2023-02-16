b84e1dad6b486a8b61c8c9673a5a10389e9d2bb3
Ilya <ikrav2003@mail.ru>
2023-02-17 01:35:15 +0300

    some not final changes in Lab2

--- src/Lab2/Main.java
+++ src/Lab2/Main.java
@@ -3,27 +3,38 @@ package Lab2;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         File fileIn = new File("input.txt");
         File fileOut = new File("output.txt");
+
         String string = "";
-        int countUpperCase = 0, countLowerCase = 0;
+        String upperString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String lowerString = "abcdefghijklmnopqrstuvwxyz";
+        Map<Character, Integer> map = new LinkedHashMap<>();
+
+        for (int i = 0; i < 26; i++) {
+            map.put(upperString.charAt(i), 0);
+            map.put(lowerString.charAt(i), 0);
+        }
+
         try (PrintWriter printWriter = new PrintWriter(fileOut)) {
             Scanner scanner = new Scanner(fileIn);
             while (scanner.hasNextLine()){
                 string = string.concat(scanner.nextLine());
             }
             for (char el : string.toCharArray()){
-                if (Character.isAlphabetic(el)){
-                    if (Character.isUpperCase(el)) { countUpperCase++; }
-                    else if (Character.isLowerCase(el)) { countLowerCase++; }
-                }
+                if (map.containsKey(el)) { map.put(el, map.get(el) + 1); }
             }
-            printWriter.println("Upper Case: " + countUpperCase);
-            printWriter.println("Lower Case: " + countLowerCase);
+
+            for (Map.Entry<Character, Integer> entry : map.entrySet()){
+                printWriter.println("The number of symbols " + entry.getKey() + " = " + entry.getValue());
+            }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
